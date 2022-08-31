b919924f5d482e0464d5a15156969ead2dad8ea6
Konstantin Filyaev <uzornakovre@yandex.ru>
2022-08-31 14:24:30 +0300

    feat: popup_script

--- script/script.js
+++ script/script.js
@@ -4,25 +4,38 @@ let popupClose = document.querySelector('.popup__close');
 let popupContainer = popup.querySelector('.popup__container');
 let nameInput = popup.querySelector('.popup__form-input_content_name')
 let jobInput = popup.querySelector('.popup__form-input_content_job')
+let submitInput = popup.querySelector('.popup__form-submit');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
-function popupOn() {
+buttonEdit.addEventListener('click', function popupOn() {
   popup.setAttribute('style', 'display: flex');
-}
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
 
-function popupOff() {
+popupClose.addEventListener('click', function popupOff(evt) {
+  evt.preventDefault();
   popup.setAttribute('style', 'display: none');
-}
+});
 
-buttonEdit.addEventListener('click', popupOn);
-popupClose.addEventListener('click', popupOff);
-
-function formSubmitHandler (evt) {
+popupContainer.addEventListener('submit', function formSubmitHandler(evt) {
     evt.preventDefault();
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popupOff();
-}
+    popup.setAttribute('style', 'display: none');
+});
 
-popupContainer.addEventListener('submit', formSubmitHandler);
+nameInput.addEventListener('keypress', function(evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 13) {
+    jobInput.focus();
+  }
+});
+
+jobInput.addEventListener('keypress', function(evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 13) {
+    submitInput.focus();
+  }
+});
