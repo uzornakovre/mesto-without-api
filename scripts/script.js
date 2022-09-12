4c47b364d834c5d92cd8e2ad391eb431e390f04f
Konstantin Filyaev <uzornakovre@yandex.ru>
2022-09-12 20:17:02 +0300

    feat: card_template

--- scripts/script.js
+++ scripts/script.js
@@ -8,24 +8,63 @@ const submitInput = popup.querySelector('.popup__form-submit');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
-function openEditPopup() {
+function openPopup () {
   popup.classList.add('popup_opened');
+  popupContainer.classList.add('popup__container_opened');
+}
+
+function openEditPopup() {
+  openPopup();
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function closeEditPopup() {
+function closePopup() {
   popup.classList.remove('popup_opened');
+  popupContainer.classList.remove('popup__container_opened');
 }
 
 buttonEdit.addEventListener('click', openEditPopup);
-popupClose.addEventListener('click', closeEditPopup);
+popupClose.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    closeEditPopup();
+    closePopup();
 }
 
 popupContainer.addEventListener('submit', formSubmitHandler);
+
+// const cardTemplate = document.querySelector('#card').content;
+// const initialCards = [
+//   {
+//     name: 'Архыз',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+//   },
+//   {
+//     name: 'Челябинская область',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+//   },
+//   {
+//     name: 'Иваново',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+//   },
+//   {
+//     name: 'Камчатка',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+//   },
+//   {
+//     name: 'Холмогорский район',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+//   },
+//   {
+//     name: 'Байкал',
+//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+//   }
+// ];
+
+// const cardElement = cardTemplate.querySelector('.elements__list-item').cloneNode(true);
+
+// cardElement.querySelector('.element__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+// cardElement.querySelector('.element__title').textContent = 'Архыз';
